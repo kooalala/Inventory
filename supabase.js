bc20fde9e770cce41be0d46dcb5f5742af8f9ef5
kooalala <ashleela0810@gmail.com>
2026-02-09 07:13:54 +0000

    keys added

--- supabase.js
+++ supabase.js
@@ -2,7 +2,7 @@ import 'react-native-url-polyfill/auto';
 import { createClient } from '@supabase/supabase-js';
 
 // ⚠️ PASTE YOUR KEYS INSIDE THE QUOTES BELOW
-const supabaseUrl = 'YOUR_SUPABASE_URL_HERE';
-const supabaseKey = 'YOUR_SUPABASE_ANON_KEY_HERE';
+const supabaseUrl = 'https://fispwtmcqcybzurtykem.supabase.co';
+const supabaseKey = 'sb_publishable_p9tJCSeKcysUUsoemIVlmQ_wgEujybo';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
